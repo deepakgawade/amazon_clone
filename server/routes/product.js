bc09513af8a3603d7b6bf342717cf6b49f28c657
deepakgaade <deepakgawade22@gmail.com>
2023-02-06 11:43:12 +0530

    product js added

--- server/routes/product.js
+++ server/routes/product.js
@@ -34,10 +34,22 @@ productRouter.get("/api/products/search/:name", auth, async (req, res) => {
 
 productRouter.post("/api/rate-product", auth, async (req, res) => {
   try {
-    const {id,rating}=req.body;
+    const { id, rating } = req.body;
 
-    
+    let product = await Product.findById(id);
 
+    for (int = 0; i < product.ratings.length; i++) {
+      if (product.ratings[i].userId == req.userId) {
+        product.ratings.splice(i, 1);
+        break;
+      }
+    }
+    const ratingSchema = { userId: req.user, rating };
+
+    product.ratings.push(ratingSchema);
+    product = await product.save();
+
+    res.json(product);
   } catch (e) {
     res.status(500).json({ error: e.message });
   }
